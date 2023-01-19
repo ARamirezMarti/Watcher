092a73cd872d33d880b15b89b6b8127a3cc368c9
Tony <tony@pop-os.localdomain>
2023-01-20 00:04:42 +0100

    Create guard clause for libraryId in Movie model

--- src/Movie/Domain/Model/Movie.php
+++ src/Movie/Domain/Model/Movie.php
@@ -7,6 +7,7 @@ use App\Movie\Domain\Model\MovieName;
 use App\Movie\Domain\Model\MovieYear;
 use App\Movie\Domain\Model\MovieUrl;
 use Ramsey\Uuid\Uuid;
+use App\Library\Domain\Exceptions\InvalidArgumentDomainException;
 
 class Movie
 {
@@ -17,22 +18,55 @@ class Movie
     private MovieUrl $MovieUrl;
     private string $LibraryId;
 
-    private function __construct(MovieName $name,  MovieYear $year, MovieUrl $movieUrl, string $LibraryId)
+    private function __construct(MovieName $name,  MovieYear $year, MovieUrl $movieUrl, string|null $LibraryId)
     {
         $this->id = Uuid::uuid4()->toString();
         $this->MovieName = $name;
         $this->MovieYear = $year;
         $this->MovieUrl = $movieUrl;
+        
+
+        $this->checkLibraryIdIsAdded($LibraryId);
         $this->LibraryId = $LibraryId;    
 
     }
 
-    public static function create(MovieName $movieName,MovieYear $movieYear,MovieUrl $movieUrl, string $LibraryId): self
+    public static function create(MovieName $movieName,MovieYear $movieYear,MovieUrl $movieUrl, string | null $LibraryId): self
     {
         return new static ($movieName,$movieYear,$movieUrl,$LibraryId);
     }
 
- 
+    private  function checkLibraryIdIsAdded( $LibraryId): void
+    {
+        if(null==$LibraryId){
+            throw InvalidArgumentDomainException::createFromMessage('LibraryId can not be null');
 
-   
+        }
+    }
+
+    public function Id(): string
+    {
+        return $this->id;
+    }
+
+
+	public function getMovieName(): MovieName {
+		return $this->MovieName;
+	}
+
+
+	public function getMovieYear(): MovieYear {
+		return $this->MovieYear;
+	}
+
+	public function getMovieUrl(): MovieUrl {
+		return $this->MovieUrl;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLibraryId(): string {
+		return $this->LibraryId;
+	}
 }
--- src/Movie/Domain/Model/MovieUrl.php
+++ src/Movie/Domain/Model/MovieUrl.php
@@ -5,13 +5,17 @@ namespace App\Movie\Domain\Model;
 class MovieUrl
 {
 
-    private const BASE_URL =  "/watch/";
+    private const BASE_URL =  "/watch";
     private $url;
 
     public function __construct(string $Url ) {
         $correctUrl = str_replace(" ", "-", $Url);
-        $this->url = self::BASE_URL . $correctUrl;
+        $this->url = self::BASE_URL ."/"    . $correctUrl;
     }
     
+    public function Value(): string 
+    {
+        return $this->url;
+    }
 
 }
